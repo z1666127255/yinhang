747590e707470b6935434893b450503ce8748e12
曾英峰 <1666127255@qq.com>
2019-01-15 15:04:29 +0800

    基本框架

--- main.c
+++ main.c
@@ -2,8 +2,31 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("欢迎进入银行管理系统\n");
+    while(1){
+        printf("1----存钱\n");
+        printf("2----取钱\n");
+        printf("3----余额\n");
+        printf("4----退出\n");
 
-    return 0;
+        printf("请输入相应功能编号\n");
+        int code;
+        scanf("%d",&code);
 
+        if(code == 1){
+
+        }
+
+        if(code == 2){
+            
+        }
+
+        if(code == 3){
+            
+        }
+
+        if(code == 4){
+            
+        }
+    }
 }
